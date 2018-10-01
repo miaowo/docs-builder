b08a9f2b26de068af954f0f193bc1373f4651f75
Katzeilla <hello@katzeilla.org>
2018-10-01 19:35:57 -0400

    Change list name & Get output directory from command line

--- mkdocs.sh
+++ mkdocs.sh
@@ -1,26 +1,35 @@
 #! /bin/bash
 
+IFS=$'\n'       # make newlines the only separator
+set -f          # disable globbing
+
+output="$1"
+
 mkInitDocs() {
 
-	display_name=$1 
-	dir_name=$2
 
 	echo -n '中文名：' 
-	echo $display_name
+	echo "$display_name"
+	echo test > "$output"/"$dir_name"/index.html
 
 	echo -n '目录名：' 
-	echo $dir_name
+	echo "$dir_name"
 
 }
 
-IFS=$'\n'       # make newlines the only separator
-set -f          # disable globbingi
+firstChar() {
+	
+	echo "$i" | cut -c 1 
+}
 
+for i in $(cat < name.list); do
 
-for i in $(cat < dirname.list); do
 	display_name=$(echo "$i" | cut -f1 -d":")
 	dir_name=$(echo "$i" | cut -f2 -d":")
-	mkInitDocs $display_name $dir_name
+
+	if [[ "$(firstChar)" != '#' ]]; then
+	      	mkInitDocs
+	fi
 done
 
 
