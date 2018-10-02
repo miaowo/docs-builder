8e33a526317e14d13cac7d47591fa71fb6100ac3
Katzeilla <hello@katzeilla.org>
2018-10-01 21:44:15 -0400

    Update mkdir.sh

--- mkdir.sh
+++ mkdir.sh
@@ -5,7 +5,7 @@ IFS=$'\n'       # make newlines the only separator
 set -f          # disable globbing
 
 
-for i in $(cat < dirname.list); do
+for i in $(cat < name.list); do
 
 	  dir_name=$(echo "$i" | cut -f2 -d":")
 	  mkdir "$output"/"$dir_name"
