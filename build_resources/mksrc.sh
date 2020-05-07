73f937a1e7668e5bc93adad8f273e3ff2c232009
Katzeilla <hello@katzeilla.org>
2020-05-07 08:11:40 +0000

    Add new script for making dummy files

---
+++ build_resources/mksrc.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+output=$1
+IFS=$'\n'       # make newlines the only separator
+set -f          # disable globbing
+
+
+for i in $(cat < name.list); do
+
+	  dir_name=$(echo "$i" | cut -f2 -d":")
+	  mkdir -p source/"$dir_name"
+	  touch source/"$dir_name"/"$dir_name".md
+done
+
